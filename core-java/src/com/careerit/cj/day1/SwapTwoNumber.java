fe6e9f9949883bc0b424c32ca48cf05eee8e3495
learnwithlakshman@gmail.com <learnwithlakshman@gmail.com>
2022-06-02 08:02:41 +0530

    Added swapping of two numbers

---
+++ core-java/src/com/careerit/cj/day1/SwapTwoNumber.java
@@ -0,0 +1,16 @@
+package com.careerit.cj.day1;
+
+public class SwapTwoNumber {
+
+		public static void main(String[] args) {
+			int num1 = 10;
+			int num2 = 20;
+			System.out.println("Before swap num1 = "+num1+" and num2 = "+num2);
+			int temp = num1;
+			num1 = num2;
+			num2 = temp;
+			System.out.println("After swap num1 = "+num1+" and num2 = "+num2);
+			
+			
+		}
+}
